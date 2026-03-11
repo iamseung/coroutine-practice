ba1e2925c01a5c8275fdd4e3e8fc14a84362dbae
이승석 <seesungseok@naver.com>
2026-03-11 17:02:40 +0900

    project setting

---
+++ src/test/kotlin/com/example/coroutinepractice/CoroutinePracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.coroutinepractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CoroutinePracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
